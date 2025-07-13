2264f56a6d9e1c12e7986552a973fb98ca26b2f3
Zabirov Rodion <rodionzabirov888@gmail.com>
2025-07-14 00:37:00 +0500

    Сделал подзадачу 5

---
+++ Class4.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubTest
+{
+    internal class Class4
+    {
+        // Изменение в подзадаче 5
+    }
+}
